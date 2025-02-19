5d27de0e36617eec889957d4ef208a08092b51b6
JosephMargulis <Joseph.margulis@gmail.com>
2025-02-18 19:04:14 -0500

    Update sliderScript.js

--- Scripts/sliderScript.js
+++ Scripts/sliderScript.js
@@ -7,50 +7,63 @@ let next = document.getElementById('next');
 let active = 0;
 let lengthItems = items.length - 1;
 
-next.onclick = function()
-{
-    if(active + 1 > lengthItems)
-        {
-            active = 0;
-        }
-        else
-        {
-            active = active + 1
-        }
-        reloadSlider()
+next.onclick = function () {
+    moveSlide("next");
+};
+
+prev.onclick = function () {
+    moveSlide("prev");
+};
+
+let refreshSlider = setInterval(() => { next.click(); }, 5000);
+
+function moveSlide(direction) {
+    if (direction === "next") {
+        active = (active + 1 > lengthItems) ? 0 : active + 1;
+    } else if (direction === "prev") {
+        active = (active - 1 < 0) ? lengthItems : active - 1;
+    }
+    reloadSlider();
 }
 
-prev.onclick = function()
-{
-    if(active - 1 < 0)
-        {
-            active = lengthItems;
-        }
-        else
-        {
-            active = active -1
-        }
-        reloadSlider()
-}
-
-let refreshSlider = setInterval(()=> {next.click()}, 5000);
-function reloadSlider()
-{
-    let checkLeft = items[active].offsetLeft
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
     list.style.left = -checkLeft + 'px';
-    
+
     let lastActiveDot = document.querySelector('.slider .dots li.active');
     lastActiveDot.classList.remove('active');
     dots[active].classList.add('active');
+
     clearInterval(refreshSlider);
-    refreshSlider = setInterval(()=> {next.click()}, 5000);
+    refreshSlider = setInterval(() => { next.click(); }, 5000);
 }
 
-dots.forEach((li, key) => 
-{
-    li.addEventListener('click', function()
-    {
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
         active = key;
-        reloadSlider()
-    })
-})
+        reloadSlider();
+    });
+});
+
+let touchStartX = 0;
+let touchEndX = 0;
+
+list.addEventListener("touchstart", (e) => {
+    touchStartX = e.touches[0].clientX;
+});
+
+list.addEventListener("touchmove", (e) => {
+    touchEndX = e.touches[0].clientX;
+});
+
+list.addEventListener("touchend", () => {
+    let swipeDistance = touchStartX - touchEndX;
+
+    if (swipeDistance > 50) {
+        moveSlide("next"); 
+    } else if (swipeDistance < -50) {
+        moveSlide("prev"); 
+    }
+});
+
+list.style.touchAction = "pan-x"; 
